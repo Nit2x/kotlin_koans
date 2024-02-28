3803b453d984bdc37f511ac55ef290a7cd0b61d9
Nit2x <anitaforpetra@gmail.com>
2024-02-28 12:45:44 +0000

    1.2

---
+++ Introduction/Hello, World.kt
@@ -0,0 +1,8 @@
+fun start(): String = "OK"
+
+fun main() {
+    println(start())
+}
+
+//Function di Kotlin diwakili dengan fun
+//Function ini akan mereturn "OK"
---
+++ Introduction/Named Arguments.kt
@@ -0,0 +1,11 @@
+fun joinOptions(options: Collection<String>) =
+    options.joinToString(prefix = "[", postfix = "]")
+
+
+fun main() {
+    println(joinOptions(listOf("a","b","c")))
+}
+
+//Collection bisa menerima listOf
+//joinToString punya prefiks dan postfiks
+
